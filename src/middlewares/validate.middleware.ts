f8622a20a22718ccdf7a105d9946ea98e56c3735
Gerasimos Alven <alvengabriel@gmail.com>
2026-06-09 12:01:06 +0700

    fixing bug

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -4,7 +4,9 @@ import { ZodType } from 'zod';
 export const validateRequest = (schema: ZodType<any, any, any>, source: 'body' | 'query' | 'params' = 'body') => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      req[source] = await schema.parseAsync(req[source]);
+      const parsed = await schema.parseAsync(req[source]);
+      Object.keys(req[source]).forEach((key) => delete req[source][key]);
+      Object.assign(req[source], parsed);
       next();
     } catch (error) {
       next(error);
